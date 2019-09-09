d34f7050f0822dbcc35db003ce3c1f33c67077f0
julian-heng <julianhengwl@gmail.com>
2019-09-09 10:10:22 +0800

    Add files

---
+++ game/src/game.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "game.h"
+
+#define SCR_WIDTH 800
+#define SCR_HEIGHT 600
+
+int main()
+{
+    GLFWwindow* win;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    if ((win = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Game", NULL, NULL)) != NULL)
+    {
+        glfwMakeContextCurrent(win);
+        glfwSetFramebufferSizeCallback(win, framebuffer_size_callback);
+
+        if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            while (! glfwWindowShouldClose(win))
+            {
+                if (glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+                    glfwSetWindowShouldClose(win, true);
+
+                glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                glfwSwapBuffers(win);
+                glfwPollEvents();
+            }
+
+            glfwTerminate();
+        }
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow* win, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
---
+++ game/src/game.h
@@ -0,0 +1,5 @@
+#ifndef GAME_H
+
+void framebuffer_size_callback(GLFWwindow*, int, int);
+
+#endif
