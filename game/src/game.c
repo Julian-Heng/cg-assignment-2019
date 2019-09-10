118498610f192f5a1b1eae7294a997864b66cc10
julian-heng <julianhengwl@gmail.com>
2019-09-10 23:35:56 +0800

    Update game.c

--- game/src/game.c
+++ game/src/game.c
@@ -10,9 +10,51 @@
 #define SCR_WIDTH 800
 #define SCR_HEIGHT 600
 
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
 int main()
 {
     GLFWwindow* win;
+
+    int success;
+    char log[BUFSIZ];
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.0f, -0.5f, 0.0f,
+        0.0f, 0.0f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.5f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.0f
+    };
+
+    unsigned int indices[] = {
+        1, 5, 3,
+        7, 5, 3,
+    };
+
+    unsigned int vao;
+    unsigned int vbo;
+    unsigned int ebo;
+
+    unsigned int vertexShader;
+    unsigned int shaderProgram;
+    unsigned int fragmentShader;
 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -23,36 +65,95 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    if ((win = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Game", NULL, NULL)) != NULL)
-    {
-        glfwMakeContextCurrent(win);
-        glfwSetFramebufferSizeCallback(win, framebuffer_size_callback);
-
-        if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-        {
-            while (! glfwWindowShouldClose(win))
-            {
-                if (glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-                    glfwSetWindowShouldClose(win, true);
-
-                glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-                glClear(GL_COLOR_BUFFER_BIT);
-
-                glfwSwapBuffers(win);
-                glfwPollEvents();
-            }
-
-            glfwTerminate();
-        }
-        else
-        {
-            fprintf(stderr, "Failed to initialise GLAD\n");
-        }
-    }
-    else
+    if ((win = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Game", NULL, NULL)) == NULL)
     {
         fprintf(stderr, "Failed to initialise window\n");
+        glfwTerminate();
+        return -1;
     }
+
+    glfwMakeContextCurrent(win);
+    glfwSetFramebufferSizeCallback(win, framebuffer_size_callback);
+
+    if (! gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        fprintf(stderr, "Failed to initialise GLAD\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (! success)
+    {
+        glGetShaderInfoLog(vertexShader, BUFSIZ, NULL, log);
+        fprintf(stderr, "%s", log);
+    }
+
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (! success)
+    {
+        glGetShaderInfoLog(fragmentShader, BUFSIZ, NULL, log);
+        fprintf(stderr, "%s", log);
+    }
+
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetShaderiv(vertexShader, GL_LINK_STATUS, &success);
+    if (! success)
+    {
+        glGetShaderInfoLog(vertexShader, BUFSIZ, NULL, log);
+        fprintf(stderr, "%s", log);
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    while (! glfwWindowShouldClose(win))
+    {
+        if (glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(win, true);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glfwTerminate();
 }
 
 void framebuffer_size_callback(GLFWwindow* win, int width, int height)
