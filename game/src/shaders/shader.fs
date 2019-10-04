a7426a85d6f7a481d9f0a7494a4bde63f37cc180
julian-heng <julianhengwl@gmail.com>
2019-10-04 16:06:39 +0800

    Attenuation

--- game/src/shaders/shader.fs
+++ game/src/shaders/shader.fs
@@ -15,9 +15,14 @@ struct Material
 struct Light
 {
     vec3 position;
+
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 // texture samplers
@@ -33,20 +38,27 @@ uniform Light light;
 
 void main()
 {
+    // attenuation
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
     // ambient
     vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoord));
+    ambient *= attenuation;
 
     // diffuse
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoord));
+    diffuse *= attenuation;
 
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * (spec * vec3(texture(material.specular, TexCoord)));
+    specular *= attenuation;
 
     // result
     FragColor = vec4(ambient + diffuse + specular, 1.0);
